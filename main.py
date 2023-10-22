37ea94c61a436bb0a097a4315cc35f58f7941624
EmiRoberti77 <emiliano.roberti@accenture.com>
2023-10-22 14:57:58 +0100

    anthropic.claude-v2

--- main.py
+++ main.py
@@ -5,21 +5,22 @@ bedrock_runtime = boto3.client(
     service_name="bedrock-runtime",
     region_name="us-east-1"
 )
-while (True):
-    prompt = input('=> ')
-    if (prompt == 'x'):
-        print('bye for now')
-        break
 
-    kwargs = {
-        "modelId": "ai21.j2-ultra-v1",
-        "contentType": "application/json",
-        "accept": "*/*",
-        "body": "{\"prompt\":\""+prompt+"\",\"maxTokens\":200,\"temperature\":0.7,\"topP\":1,\"stopSequences\":[],\"countPenalty\":{\"scale\":0},\"presencePenalty\":{\"scale\":0},\"frequencyPenalty\":{\"scale\":0}}"
-    }
-    response = bedrock_runtime.invoke_model(**kwargs)
-    response_body = json.loads(response.get('body').read())
-    print(response_body)
-    print("________________________________________________")
-    completion = response_body.get('completions')[0].get('data').get('text')
-    print(completion)
+prompt = """here is the text between the <text></text> tags
+<text>i have been to the beach and had ice cream</text>
+Give me a title to put in these tags <title></title>""".encode('unicode_escape').decode('utf-8')
+
+
+kwargs = {
+    "modelId": "anthropic.claude-v2",
+    "contentType": "application/json",
+    "accept": "*/*",
+    "body": "{\"prompt\":\"Human: "+prompt+"\\nAssistant:\",\"max_tokens_to_sample\":300,\"temperature\":1,\"top_k\":250,\"top_p\":0.999,\"stop_sequences\":[\"\\n\\nHuman:\"],\"anthropic_version\":\"bedrock-2023-05-31\"}"
+}
+
+response = bedrock_runtime.invoke_model(**kwargs)
+response_body = json.loads(response.get('body').read())
+print(response_body)
+print("________________________________________________")
+completion = response_body.get('completion')
+print(completion)
